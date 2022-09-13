2d9279cd31398fe28dd153d8dc4b9090a6eb30a1
Odunosh Moses <Odunoshomoses@gmail.com>
2022-09-13 14:48:08 +0000

    [feat]: nested components, props, and destructuring

--- productivity-tool/src/App.js
+++ productivity-tool/src/App.js
@@ -1,48 +1,61 @@
 import React from 'react';
+import Devs from './Devs.jsx';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "LinuxJobber"
-    }
-
-    this.handleChange = (e) => {
-      this.setState({
-        name: e.target.value
-      })
-    }
-
-    this.handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(this.state.name + "submitted this form");
-    }
-
-    this.handleCopy = (e) => {
-      const alert = document.getElementById("alertme");
-      alert.innerHTML = "That's an act of plagiarism";
-    }
-
-    this.handleCut = () => {
-      const alert = document.getElementById("alertme");
-      alert.innerHTML = "Stop trying to cut my content away!";
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Hello {this.state.name}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Enter Name" onChange={this.handleChange}/>
-          <button>Submit</button>
-          <p onCopy={this.handleCopy} onCut={this.handleCut}>Don't copy my content.</p>
-          <i id="alertme"></i>
-        </form>
+        <Devs />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+/*constructor() {
+  super();
+  this.state = {
+    name: "LinuxJobber"
+  }
+
+  this.handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  this.handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.name + "submitted this form");
+  }
+
+  this.handleCopy = (e) => {
+    const alert = document.getElementById("alertme");
+    alert.innerHTML = "That's an act of plagiarism";
+  }
+
+  this.handleCut = () => {
+    const alert = document.getElementById("alertme");
+    alert.innerHTML = "Stop trying to cut my content away!";
+  }
+}
+
+
+<h1>Hello {this.state.name}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Enter Name" onChange={this.handleChange}/>
+          <button>Submit</button>
+          <p onCopy={this.handleCopy} onCut={this.handleCut}>Don't copy my content.</p>
+          <i id="alertme"></i>
+        </form>*/
---
+++ productivity-tool/src/Devs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Devs extends React.Component {
+    render() {
+        return(
+            <div className="Devs">
+            <div>Moscode</div>
+            <div>25</div>
+            <div>ReactJS</div>
+            </div>
+        )
+    }
+}
+
+export default Devs;
