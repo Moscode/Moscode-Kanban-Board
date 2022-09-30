b4a6e19cb008324dfcba4d44e4c087b7cb5d16d9
Odunosh Moses <Odunoshomoses@gmail.com>
2022-09-30 15:11:29 +0000

    added packages

--- chatscrum/src/components/tasks/tasks.jsx
+++ chatscrum/src/components/tasks/tasks.jsx
@@ -2,24 +2,42 @@ import React from 'react'
 import taskLists from '../../static/tasks';
 import '../scrumboard/scrumboard.css';
 
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+
 function Tasks() {
   return (
     <div className='tasker'>
+    <DragDropContext>
         <div className="container">
-          <div className="weekly box">
-            <h4 className="task">Weekly Task</h4>
-            {taskLists.map(({id, item}, index) => {
-              return (
-              <p className='tasklist' key={id}>
-                    {item}
-                </p>
-            )
-          })}
-          </div>
-          <div className="daily box">
+          <Droppable droppableId="tasket">
+            {(provided)=>(
+              <div className="weekly box" {...provided.droppableProps} ref={provided.innerRef}>
+              <h4 className="task">Weekly Task</h4>
+              {taskLists.map(({id, item}, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                         <p style={{color: "#000"}} className="tasklist" key={id} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                         {item}
+                        </p>
+                    )}
+                    {provided.placeholder}
+                  </Draggable>
+              )
+            })}
+            </div>
+            )}
+          </Droppable>
+
+         <Droppable id="tasketer">
+          {(provided) => (
+            <div className="daily box" {...provided.droppableProps} ref={provided.innerRef}>
             <h4 className="task">Daily Task</h4>
           </div>
+          )}
+         </Droppable>
         </div>
+    </DragDropContext>
     </div>
   )
 }
